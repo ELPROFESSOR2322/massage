ebd2c9bf666d1bdc7acb9e3e8fad8f050a382e75
ELPROFESSOR2322 <anonyprofessor@gmail.com>
2024-09-30 23:51:31 +0300

    Add files via upload

---
+++ massage_anon.py
@@ -0,0 +1,29 @@
+import os 
+import webbrowser
+import time
+
+time.sleep(5)
+print("massage from professor anon")
+
+os.system("echo slav ava azm mn ava bota frekr to noka ye dxini mana chand xusha to hama chna nza ch bezhm de yalla wara telegrame >> unknown.txt") 
+time.sleep(5)
+
+
+print("please wait...")
+time.sleep(3)
+print("now open the massage")
+time.sleep(20)
+
+os.system("unknown.txt")
+print("you see..")
+def web():
+    run_windows=1
+    open_url1="https://web.telegram.org/a/#7351878876"
+    for i in range(run_windows):
+        webbrowser.open(open_url1)
+
+
+
+
+
+
